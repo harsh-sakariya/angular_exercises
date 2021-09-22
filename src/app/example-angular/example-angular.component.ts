c208822f5ce5b6f1424c269d14dc9715e68f4b3e
harsh-sakariya <harshs@prominentpixel.com>
2021-09-22 11:36:53 +0530

    exercise-2 of chapter-2 [2) create component with selector name 'example-angular' and class name 'ExeAngularComp' and use it in app component ]

---
+++ src/app/example-angular/example-angular.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "example-angular",
+    templateUrl: "./example-angular.component.html"
+})
+
+export class ExeAngularComp{
+
+}
