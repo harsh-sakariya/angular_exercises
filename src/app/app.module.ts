6bb7b750e7a986b4c34ed74deef7d7e72a3ed415
harsh-sakariya <harshs@prominentpixel.com>
2021-09-22 11:47:05 +0530

    exercise-3 of chapter-2 [3) create component  'user-example' ->  write  three dummy paragraph(<p></p>)  and give different color to all paragraph  (all code should be written in user-example.component.ts file { do not  use 'user-example.component.html' or 'user-example.component.css'  })]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ExeAngularComp } from './example-angular/example-angular.component'
+import { ExeAngularComp } from './example-angular/example-angular.component';
+import { UserExampleComponent } from './user-example/user-example.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExeAngularComp,
+    UserExampleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ExeAngularComp
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
