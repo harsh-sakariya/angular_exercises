24e2c3af5b480c247858cf9aa95e8111f78b1141
harsh-sakariya <harshs@prominentpixel.com>
2021-09-22 13:26:12 +0530

    exercise-6 of chapter-2 [6) use this 'https://www.npmjs.com/package/angular-dropdown' component in any example]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularDropdownModule } from 'angular-dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { UserExampleComponent } from './user-example/user-example.component'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularDropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
