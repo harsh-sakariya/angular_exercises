8e68aed53c808fdafd835e3762c8da2682d83d53
harsh-sakariya <harshs@prominentpixel.com>
2021-09-24 14:50:31 +0530

    exercise-3 of chapter-5 [3) create component lifecycle example and implement all method with example(also write when your lifecycle method will be called in comment as per your example , also use viewChild and contentChild in same examp ]

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,20 +1,83 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  OnChanges, 
+  DoCheck, 
+  SimpleChanges, 
+  AfterContentInit, 
+  AfterContentChecked, 
+  AfterViewInit, 
+  AfterViewChecked, 
+  OnDestroy, 
+  ViewChild, 
+  ElementRef,
+  ContentChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements OnInit {
-  usersData = [];
+export class AdminComponent implements 
+OnInit,
+OnChanges, 
+DoCheck, 
+AfterContentInit, 
+AfterContentChecked, 
+AfterViewInit, 
+AfterViewChecked, 
+OnDestroy
+{
+  @Input() user: {firstName: string, familyName: string, countryName: string};
+  @ViewChild('showData') userInfo: ElementRef;
+  @ContentChild('contentPara',{static: true}) contentPara: ElementRef;
+
+  constructor() {
+  }
   
-  storeUserData(data: {firstName: string, familyName: string, countryName: string}){
-    this.usersData.push(data);
+  //run when @Input() modified otherwise not
+  ngOnChanges(changes: SimpleChanges){
+    console.log("ngOnChanges called");
+    console.log(changes);
   }
 
-  constructor() { }
-
+  //If ngOnChanges is available then it's run immediately after ngOnChanges else it's run after constructor
   ngOnInit(): void {
+    console.log("ngOnInit called");
+  }	
+
+  //It's called immediately after ngOnInit and every change after ngOnChanges 
+  ngDoCheck(){
+    console.log("ngDoCheck called");
+  }
+
+  //call after ngDoCheck 
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit called");
+    console.log(this.contentPara.nativeElement.textContent);
+  }
+
+  //call after ngAfterContentInit
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked called");
+  }
+
+  //call after ngAfterContentChecked
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit called");
+    console.log(this.userInfo.nativeElement.textContent);
+  }
+
+  //call after ngAfterViewInit
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked called");
+  }
+
+  //call before angular destroy directive
+  ngOnDestroy(){
+    console.log("ngOnDestroy called");
   }
 
 }
