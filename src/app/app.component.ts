ff9cce8d2572519eb9b0574601e4ec074ddebec3
harsh-sakariya <harshs@prominentpixel.com>
2021-09-27 11:44:38 +0530

    clean code for new exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  usersData = [{firstName: "test", familyName: "test", countryName: "test"}];
-  
-  storeUserData(data: {firstName: string, familyName: string, countryName: string}){
-    this.usersData.push(data);
-  }
 
-  destroyData(){
-    this.usersData.splice(0,1);
-  }
-  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ParentComponent } from './parent/parent.component';
-import { ChildComponent } from './child/child.component';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ParentComponent,
-    ChildComponent,
-    UserComponent,
-    AdminComponent
   ],
   imports: [
     BrowserModule,
