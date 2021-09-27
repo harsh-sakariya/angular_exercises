32c7082e38a5b472833f8ffd06f3e6af74e4490f
harsh-sakariya <harshs@prominentpixel.com>
2021-09-27 13:36:41 +0530

    exercise of chpater-9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { StudentLoginService } from './studentLogin.service';
+import { Student } from './shared/student.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [StudentLoginService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private studentLoginService: StudentLoginService) {}
 
+  ngOnInit(){
+    this.studentLoginService.studentAdded.subscribe(
+      (students: Student[]) => {
+        console.log(students[students.length-1]);
+      }
+    )
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    StudentComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
