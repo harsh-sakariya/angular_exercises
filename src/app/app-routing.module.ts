e69c3e244c3073fef8b707b3544422a9916dc81e
harsh-sakariya <harshs@prominentpixel.com>
2021-09-28 19:27:37 +0530

    exercise of chapter-11 (not completed)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditJobComponent } from './jobs/edit-job/edit-job.component';
+import { JobDetailsComponent } from './jobs/job-details/job-details.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { PostJobComponent } from './jobs/post-job/post-job.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent},
+  { path: 'job', component: JobsComponent, children: [
+    {path: 'new', component: PostJobComponent},
+    {path: ':id', component: JobDetailsComponent},
+    {path: ':id/edit', component: EditJobComponent}
+  ]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
