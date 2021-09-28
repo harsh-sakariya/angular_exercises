64e5ceebc8958020d075445bb20528acffaa8836
harsh-sakariya <harshs@prominentpixel.com>
2021-09-28 14:23:46 +0530

    clean the code for new exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,14 @@
 import { Component, OnInit} from '@angular/core';
-import { StudentLoginService } from './studentLogin.service';
-import { Student } from './shared/student.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [StudentLoginService]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  constructor(private studentLoginService: StudentLoginService) {}
+  constructor() {}
 
   ngOnInit(){
-    this.studentLoginService.studentAdded.subscribe(
-      (students: Student[]) => {
-        console.log(students[students.length-1]);
-      }
-    )
+    
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudentComponent } from './student/student.component';
-import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
