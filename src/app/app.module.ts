d76d428c61e115b2b42b173a54d6320ed800f8db
harsh-sakariya <harshs@prominentpixel.com>
2021-09-29 15:31:22 +0530

    exercise of chapter-13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,37 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { JobsComponent } from './jobs/jobs.component';
-import { LoginComponent } from './login/login.component';
-import { PostJobComponent } from './jobs/post-job/post-job.component';
-import { JobListComponent } from './jobs/job-list/job-list.component';
-import { JobDetailsComponent } from './jobs/job-details/job-details.component';
-import { EditJobComponent } from './jobs/edit-job/edit-job.component';
-import { AuthGuard } from './auth-guard.service';
-import { AuthService } from './auth.service';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    JobsComponent,
-    LoginComponent,
-    PostJobComponent,
-    JobListComponent,
-    JobDetailsComponent,
-    EditJobComponent,
-    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [AuthGuard, AuthService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
