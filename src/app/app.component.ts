b6bbcc1922f1c461e86520d882f87c973f488165
harsh-sakariya <harshs@prominentpixel.com>
2021-10-06 14:58:36 +0530

    clean the code for next exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy{
-  strArr: string[] = ['str1', 'str2', 'str3', 'str4', 'str5', 'str6', 'str7', 'str8', 'str9'];
 
-  private stringObsSubscription: Subscription;
-
-  private customRandomStringPrinter = Observable.create(observer => {
-    let str = this.strArr[Math.floor(Math.random()*this.strArr.length)];
-    setInterval(()=>{
-      observer.next(str);
-      if(str === this.strArr[3]){
-        observer.error(new Error('do not use it'));
-      }
-      if(str === this.strArr[this.strArr.length-2]){
-        observer.complete();
-      }
-      str = this.strArr[Math.floor(Math.random()*this.strArr.length)];
-    }, 1500);
-  })
-
-  ngOnInit(){
-    this.stringObsSubscription = this.customRandomStringPrinter.subscribe(
-      (str) => {
-        console.log(str);
-      }
-    )
-  }
-
-  ngOnDestroy(){
-    this.stringObsSubscription.unsubscribe();
-  }
-
-  
+export class AppComponent{
+ 
 }
