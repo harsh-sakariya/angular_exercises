a80c31f1a65ebe9a5060223214fb74d72c0ec098
harsh-sakariya <harshs@prominentpixel.com>
2021-10-11 19:39:21 +0530

    exercise of chapter-18 40% completed

--- src/app/_service/employee.service.ts
+++ src/app/_service/employee.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
+
+import { DataStorageService } from "./data-storage.service";
 import { Employee } from "../shared/employee.model";
 
 @Injectable({providedIn: 'root'})
@@ -7,8 +9,22 @@ export class EmployeeService{
   employees: Employee[] = [];
   employeeAdded = new Subject<Employee[]>();
 
+  constructor(private dataStorageService: DataStorageService) {
+    
+  }
+
   addEmployee(employee: Employee){
-    this.employees.push(employee);
-    this.employeeAdded.next(this.employees);
+    this.dataStorageService.addData(employee).subscribe(
+      response => {
+        console.log(response);
+        this.dataStorageService.getData().subscribe(
+          (responseData) => {
+            this.employeeAdded.next(responseData);
+          }
+        )
+      }
+    )
+    // this.employees.push(employee);
+    // this.employeeAdded.next(this.employees);
   }
 }
