bff21fe1c327c22471118e9591ed490ceda62379
harsh-sakariya <harshs@prominentpixel.com>
2021-10-12 14:50:43 +0530

    exercise of chpater-21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AlertComponent } from './alert/alert.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-root',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 
-export class AppComponent implements OnInit{
-  constructor() {}
+export class AppComponent {
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective; 
+  private closeSub: Subscription;
 
-  ngOnInit(){
-    
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+  onShowModal(){
+    const alertCmpFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(alertCmpFactory);
+    this.closeSub = componentRef.instance.close.subscribe(
+      () => {
+        this.closeSub.unsubscribe();
+        hostViewContainerRef.clear();
+      }
+    )
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert/alert.component';
+import { PlaceholderDirective } from './placeholder/placeholder.directive';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -20,6 +24,7 @@ import { AppComponent } from './app.component';
     HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AlertComponent]
 })
 export class AppModule { }
