47790e954bef25e9089a0c2851cb038bc5d1afa2
harsh-sakariya <harshs@prominentpixel.com>
2021-10-12 13:56:14 +0530

    clean code for next exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataStorageService } from './_service/data-storage.service';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +7,7 @@ import { DataStorageService } from './_service/data-storage.service';
 })
 
 export class AppComponent implements OnInit{
-  constructor(private dataStorageService: DataStorageService) {}
+  constructor() {}
 
   ngOnInit(){
     
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { EmployeeFormComponent } from './employee-form/employee-form.component';
-import { AuthInterceptorService } from './_service/auth-interceptor.service';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeListComponent,
-    EmployeeFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -24,13 +19,7 @@ import { AuthInterceptorService } from './_service/auth-interceptor.service';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
